8dacc02137bbfbe411cdc620c7332ae95c0f983a
davidblancolopez <daviddbl94@gmail.com>
2017-05-25 16:49:13 +0200

    CAMBIOS.

--- src/exercici/pkg1/m3/uf6/Exercici1M3UF6.java
+++ src/exercici/pkg1/m3/uf6/Exercici1M3UF6.java
@@ -1,12 +1,16 @@
 
 package exercici.pkg1.m3.uf6;
 
+import Control.GestioUsuaris;
+
 
 public class Exercici1M3UF6 {
 
 
     public static void main(String[] args) {
+        GestioUsuaris gu = new GestioUsuaris();
         
+        gu.iniciarPrograma();
     }
     
 }
